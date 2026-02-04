8aa713d00e509130186197fc1967ce17aab57e08
DDamaris7 <damadominguez23@gmail.com>
2026-02-04 20:01:26 -0300

    Se agrega un item mas para el listado, se agrega una libreria y se modifica el json

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,6 @@
-// PRESENTACIÓN DEL SIMULACRO A TRAVES DE HTML.
-let inventario = JSON.parse(localStorage.getItem("inventario")) || [];
+// PRESENTACIÓN DEL SIMULACRO A TRAVES DE HTML 
+
+let inventario = [];
 let idEditando = null;
 
 //  DOM 
@@ -8,6 +9,7 @@ const total = document.getElementById("total");
 const mensaje = document.getElementById("mensaje");
 const editNombre = document.getElementById("edit-nombre");
 const editCantidad = document.getElementById("edit-cantidad");
+const editCodigo = document.getElementById("edit-codigo");
 const btnGuardar = document.getElementById("btn-guardar");
 const tituloEditar = document.getElementById("titulo-editar");
 
@@ -15,27 +17,61 @@ const tituloEditar = document.getElementById("titulo-editar");
 function guardarStorage() {
   localStorage.setItem("inventario", JSON.stringify(inventario));
 }
+ 
+//Carga de datos inicial
+const inventarioStorage = JSON.parse(localStorage.getItem("inventario"));
+
+ if (inventarioStorage) {
+  inventario = inventarioStorage;
+  mostrarInventario();
+} else {
+  fetch("data.json")
+    .then(res => res.json())
+    .then(data => {
+      inventario = data;
+      guardarStorage();
+      mostrarInventario();
+    });
+}
+
 
 // AGREGAR PRODUCTO
 function agregarProducto() {
-  const nombre = document.getElementById("nombre").value;
+  const nombre = document.getElementById("nombre").value.trim();
   const cantidad = Number(document.getElementById("cantidad").value);
+  const codigo = Number(document.getElementById("codigo").value); 
 
   if (nombre === "" || cantidad <= 0) {
     mensaje.textContent = "Datos inválidos";
-    return;
-  }
+   return;
+ }
 
-  mensaje.textContent = "";
+// Eliminar productos duplicados
+const existe = inventario.some(p => p.nombre.toLowerCase() === nombre.toLowerCase());
+if (existe) {
+  mensaje.textContent = "El producto ya existe";
+  return;
+}
 
-  inventario.push({
+ inventario.push({
     id: Date.now(),
     nombre,
-    cantidad
+    cantidad,
+    codigo,
   });
 
   guardarStorage();
   mostrarInventario();
+  limpiarInputs ();
+ 
+  Swal.fire("Producto agregado correctamente");
+}
+// mensaje.textContent = "";
+
+
+//Limpiar inputs
+function limpiarInputs() {
+  document.querySelectorAll("input").forEach(input => input.value = "");
 }
 
 // MOSTRAR LISTA
@@ -45,21 +81,32 @@ function mostrarInventario() {
   inventario.forEach(producto => {
     const li = document.createElement("li");
     li.innerHTML = `
-      ${producto.nombre} - ${producto.cantidad} unidades
+      ${producto.codigo}-${producto.nombre} - ${producto.cantidad} unidades
       <button onclick="editarProducto(${producto.id})">✏️</button>
       <button onclick="eliminarProducto(${producto.id})">❌</button>
     `;
     lista.appendChild(li);
   });
+  
+  total.textContent = `Total de productos: ${inventario.length}`;
 
-  calcularTotal();
 }
 
 // ELIMINAR PRODUCTO
 function eliminarProducto(id) {
-  inventario = inventario.filter(p => p.id !== id);
-  guardarStorage();
-  mostrarInventario();
+  Swal.fire({
+    title: "¿Eliminar producto?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, eliminar",
+    cancelButtonText: "Cancelar"
+  }).then(result => {
+    if (result.isConfirmed) {
+      inventario = inventario.filter(p => p.id !== id);
+      guardarStorage();
+      mostrarInventario();
+    }
+  });
 }
 
 // EDITAR PRODUCTO
@@ -68,26 +115,28 @@ function editarProducto(id) {
   idEditando = id;
   editNombre.value = producto.nombre;
   editCantidad.value = producto.cantidad;
+  editCodigo.value = producto.codigo;
   editNombre.style.display = "inline";
   editCantidad.style.display = "inline";
+  editCodigo.style.display = "inline";
   btnGuardar.style.display = "inline";
   tituloEditar.style.display = "block";
 }
 
 btnGuardar.addEventListener("click", () => {
-  const nuevoNombre = editNombre.value;
+  const nuevoNombre = editNombre.value.trim();
   const nuevaCantidad = Number(editCantidad.value);
+  const nuevoCodigo = Number(editCodigo.value);
 
   if (nuevoNombre === "" || nuevaCantidad <= 0) {
     mensaje.textContent = "Datos inválidos";
     return;
   }
 
-  mensaje.textContent = "";
 
   inventario = inventario.map(p =>
     p.id === idEditando
-      ? { ...p, nombre: nuevoNombre, cantidad: nuevaCantidad }
+      ? { ...p, nombre: nuevoNombre, cantidad: nuevaCantidad, codigo: nuevoCodigo}
       : p
   );
 
@@ -97,17 +146,13 @@ btnGuardar.addEventListener("click", () => {
   // Ocultar edición
   editNombre.style.display = "none";
   editCantidad.style.display = "none";
+  editCodigo.style.display = "none";
   btnGuardar.style.display = "none";
   tituloEditar.style.display = "none";
 });
 
-//TOTAL DE PRODUCTOS EN LISTA
-function calcularTotal() {
-  const totalProductos = inventario.length;
-  total.textContent = `Total de productos: ${totalProductos}`;
-}
 
-mostrarInventario();
+
 
 
 
